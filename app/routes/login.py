177205a0b31df14825a594dc6b7342c291263389
WilliamMiguel <113279478+WilliamMiguel@users.noreply.github.com>
2022-11-26 15:36:16 -0500

    Elimnando env

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from app.models.user import User
 from app.utils.db import db
-import requests
 
 login = Blueprint('login',__name__)
